aa4db36c821617a830a7c4d766ab0e4208959e10
Sandesh Rana <vimmrana0@gmail.com>
2023-06-07 09:13:41 +0545

    Fix django server runtime with settimeout

--- webdesk-de/main.js
+++ webdesk-de/main.js
@@ -1,81 +1,52 @@
-const { app, BrowserWindow } = require('electron')
-const path = require('path')
-const {spawn} = require('child_process');
+const { app, BrowserWindow } = require('electron');
+const { spawn } = require('child_process');
 
-let DJANGO_CHILD_PROCESS = null;
-const MAIN_WINDOW_WEBPACK_ENTRY = '';
+process.env['ELECTRON_DISABLE_SECURITY_WARNINGS'] = 'true';
+let dj;
+let mainWindow = null;
 
-const spawnDjango = () =>
-{
-    // if (isDevelopmentEnv()) {
-    //     return spawn(`python\\edtwExampleEnv\\Scripts\\python.exe`,
-    //         ['python\\edtwExample\\manage.py', 'runserver', '--noreload'], {
-    //         shell: true,
-    //     });
-    // }
-    return spawn(`cd back && webdeskbackend.exe runserver --noreload`, {
-        shell: true,
+const startDjango = () => {
+    // return new Promise((resolve) => {
+    dj = spawn(`./back/webdeskbackend runserver --noreload`, { shell: true });
+
+    dj.stdout.on('data', (data) => {
+        console.log(`stdout: ${data}`);
     });
-}
 
-const startDjangoServer = () =>
-{
-    DJANGO_CHILD_PROCESS = spawnDjango();
-}
+    dj.stderr.on('data', (data) => {
+        console.log(`stderr: ${data}`);
+    });
 
-const createWindow = () => {
-    startDjangoServer();
-    // Create the browser window.
-    // const mainWindow = new BrowserWindow({
-    //     height: 600,
-    //     width: 800,
+    dj.on('close', (code) => {
+        console.log(`child process exited with code ${code}`);
+    });
     // });
-
-    // mainWindow.webContents.session.webRequest.onBeforeSendHeaders(
-    //     (details, callback) =>
-    //     {
-    //         const { requestHeaders } = details;
-    //         UpsertKeyValue(requestHeaders, 'Access-Control-Allow-Origin', ['*']);
-    //         callback({ requestHeaders });
-    //     },
-    // );
-
-    // mainWindow.webContents.session.webRequest.onHeadersReceived((details, callback) =>
-    // {
-    //     const { responseHeaders } = details;
-    //     UpsertKeyValue(responseHeaders, 'Access-Control-Allow-Origin', ['*']);
-    //     UpsertKeyValue(responseHeaders, 'Access-Control-Allow-Headers', ['*']);
-    //     callback({
-    //         responseHeaders,
-    //     });
-    // });
-
-    // // and load the index.html of the app.
-    // mainWindow.loadURL(MAIN_WINDOW_WEBPACK_ENTRY);
-
-    // Open the DevTools.
-    // openDevTools(mainWindow);
-    const win = new BrowserWindow({
-        width: 800,
-        height: 600,
-        // webPreferences: {
-        //     preload: path.join(__dirname, 'preload.js')
-        // }
-    })
-
-    // win.loadFile('index.html')
-    win.loadURL('http://localhost:8000/admin')
-
-}
-
+};
 
 app.whenReady().then(() => {
-    createWindow()
+    startDjango();
 
-    app.on('activate', () => {
-        if (BrowserWindow.getAllWindows().length === 0) createWindow()
-    })
+    setTimeout(() => {
+        mainWindow = new BrowserWindow({ width: 800, height: 600 });
+        mainWindow.loadURL('http://localhost:8000/admin');
+        mainWindow.on('closed', function () {
+            mainWindow = null;
+            dj.kill('SIGINT');
+        });
+    }, 1000);
+});
+
+
+// Kill local Django server
+app.on('before-quit', function () {
+    dj.kill('SIGINT')
 })
-app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') app.quit()
+
+// Quit when all windows are closed.
+app.on('window-all-closed', function () {
+
+    // On macOS it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
+    if (process.platform !== 'darwin')
+        app.quit()
 })
